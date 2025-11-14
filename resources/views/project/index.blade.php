4ec86311d80fb0234b45675b431443661202a7ac
BranGitfox <vixfgit@gmail.com>
2025-11-14 21:10:31 +0300

    style:design project page

---
+++ resources/views/project/index.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+
+    <x-slot:title>Project</x-slot:title>
+    <div class="layout-container flex h-full grow flex-col">
+        <x-header/>
+        <livewire:project/>
+    </div>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TasksController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
@@ -18,5 +19,6 @@ Route::get('/task/create', [TasksController::class, 'create'])->middleware('auth
 Route::post('/task/create', [TasksController::class, 'store'])->middleware('auth');
 Route::get('/task/{task}/edit', [TasksController::class, 'show'])->middleware('auth')->can('edit', 'task');
 Route::post('/task/{task}/edit', [TasksController::class, 'edit'])->middleware('auth')->can('edit', 'task');
+Route::get('/projects', [ProjectController::class, 'index'])->middleware('auth');
 Route::get('/notify', [TasksController::class, 'notify'])->middleware('auth');
 
